2afcca8248938af437c2328c4e59e2dd21710379
Nibin K <nibin9797@gmail.com>
2021-10-12 01:26:33 +0530

    Updated the Entity - User

--- bookmarker_backend/src/main/java/com/project/bookmarker/model/User.java
+++ bookmarker_backend/src/main/java/com/project/bookmarker/model/User.java
@@ -13,9 +13,60 @@ public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private String username;
+	private long userId;
+	private String email;
 	private String name;
 	private String password;
 	private boolean dark;
-	private String bookmarks;
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isDark() {
+		return dark;
+	}
+
+	public void setDark(boolean dark) {
+		this.dark = dark;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"userId=" + userId +
+				", email='" + email + '\'' +
+				", name='" + name + '\'' +
+				", password='" + password + '\'' +
+				", dark=" + dark +
+				'}';
+	}
 }
